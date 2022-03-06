34970a3af16c69307b915f91b88a0862682f9390
向军(xiangjun02) <>
2022-03-06 23:57:12 +0800

    refactor: nothing

--- src/extension.ts
+++ src/extension.ts
@@ -3,11 +3,12 @@
  * @Author: xiangjun
  * @Date: 2021-11-18 09:26:07
  * @LastEditors: xiangjun02
- * @LastEditTime: 2022-03-06 23:43:00
+ * @LastEditTime: 2022-03-06 23:57:04
  */
 import { ExtensionContext, window } from "vscode";
 // easySnippet
 import SnippetTool from './easy-snippet/index'
+
 // 读取当前的terminal
 export const getTerminal = (terminalName: string) => {
   return window.terminals.find((t) => t.name === terminalName);
